51267349fde1b9c5d339f28557a9690411b28638
2008020301 <2008020301@edu.odulphus.nl>
2026-04-02 10:57:57 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,35 @@
 import json
-with open("PO1/results.json") as data:
-    data = json.load(data)
+import matplotlib.pyplot as plt
+from pandas import DataFrame as DF
 
-print(data[1])
+with open("PO1/results.json") as data:
+    data = json.load(data) #Laadt de dataset
+
+names = [(data[i]["name"]) for i in range(len(data))] #Maakt lijst met sporten aan
+
+table = DF(names, columns=["Sports"])
+#print(table) #Maakt lijst met namen overzichtelijk bij printen
+
+'''while True:
+    try:
+        index = int(input("0-46: "))
+        if index > 46:
+            raise ValueError    
+        break
+    except:
+        pass'''
+
+#years = [(data[index]["games"][i]["year"]) for i in range(len(data[index]["games"]))]
+#print(years) #x-as
+
+#first_place = (data[0]["games"][0]["results"][0]["result"]) #laadt eerste plek
+#years = (data[0]["games"][0]["year"])
+
+#x, y = years, first_place
+#print(x, y)
+
+for i in (data[0]["games"]):
+    print(i)
+    break
+
+#print(len(data[0]["games"][0]["results"]))
