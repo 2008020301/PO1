d9b44bce414500fefbd866c2ff398712ce714f75
2008020301 <2008020301@edu.odulphus.nl>
2026-04-14 12:54:36 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import json
 import matplotlib.pyplot as plt
 import pandas as pd
+import re
 
 with open("PO1/results.json") as data:
     data = json.load(data) #Laadt de dataset
+
+for n in range(len(data)):
+    print(f"{n}. {data[n]['name']}")
 
 while True: #Vraagt de gebruiker om een sport
     try:
@@ -11,13 +15,43 @@ while True: #Vraagt de gebruiker om een sport
         if sport > 46:
             raise ValueError    
         break
-    except ValueError:
+    except:
         pass
 
 y = []; g = []; s = []; b = [] #Maakt lijsten met tijden en jaren aan
 for i in (data[sport]["games"]):
-    u, v, w, x = i["year"], i["results"][0]["result"], i["results"][1]["result"], i["results"][2]["result"]
-    y.append(u); g.append(v); s.append(w); b.append(x)
+    try:
+        u = i["year"]
+        gsb = ["none", "none", "none"]
+        for n in range(len(i["results"])):
+            gsb[n] = i["results"][n]["result"]
+        v, w, x = gsb[0], gsb[1], gsb[2]
+    except IndexError:
+        print(i)
+
+    y.append(u)
+    stringsplit = re.compile(r"\d+").findall
+    
+    def result_correction(n:str):
+        if ",+" in n:
+            n = n.split(",+")[0]
+        return stringsplit(n)
+    
+    v = result_correction(str(v))
+    w = result_correction(str(w))
+    x = result_correction(str(x))
+
+    def string_to_seconds(str):
+        conversion = [3600, 60, 1, 0.01]
+        seconds = 0
+        for i, _ in enumerate(str):
+            seconds += int(str[-(i+1)]) * conversion[-(i+1)]
+        return seconds
+
+    g.append(string_to_seconds(v))
+    s.append(string_to_seconds(w))
+    b.append(string_to_seconds(x))
+
 
 timetable = pd.DataFrame()
 timetable = timetable.assign( #Maakt het tabel met pandas aan
