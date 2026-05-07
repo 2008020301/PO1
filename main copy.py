182dda42c837a5d4b05d917d6e1e20d61d456110
2008020301 <2008020301@edu.odulphus.nl>
2026-05-07 10:58:37 +0200

    Add files via upload

--- main copy.py
+++ main copy.py
@@ -18,16 +18,14 @@ def sport_names(data):
         print(f"{n}. {names[n]}")
         return n
 
-def user_input(data, i):
+def user_input(data):
     while True: #Vraagt de gebruiker om een sport
         try:
-            sport = i
+            sport = int(input("0-44: "))
             if sport > 44:
                 raise ValueError  
-            if 13 < sport <= 38:
+            if sport == 14 or sport == 39:
                 sport += 1
-            elif sport > 38:
-                sport += 2
             break
         except:
             pass
@@ -54,78 +52,74 @@ def result_correction(n:str):
     stringsplit = re.compile(r"\d+").findall
     return stringsplit(n)
 
-for i in range(44):
-    sport_names(data)
-    year, sport = user_input(data, i)
 
-    print(sport)
-    if sport in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 19, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 41]:
-        year = quick_sort(year)
-        gold = [[y[0], result_correction(y[1][0])] for y in year]
-        silver = [[y[0], result_correction(y[1][1])] for y in year]
-        bronze = [[y[0], result_correction(y[1][1])] for y in year]
-        print(gold, silver, bronze)
-        
-    elif sport in [14, 15, 16, 17, 18, 20, 21, 22, 36, 37, 38, 39, 40, 42, 43, 44]:
-        for y in year:
-            y[1][0] = float(y[1][0])
-            y[1][1] = float(y[1][1])
-            y[1][2] = float(y[1][2])
+sport_names(data)
+year, sport = user_input(data,)
+print(sport)
+if sport in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 19, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 41]:
+    year = quick_sort(year)
+    gold = [[y[0], result_correction(str(y[1][0]))] for y in year]
+    silver = [[y[0], result_correction(str(y[1][1]))] for y in year]
+    bronze = [[y[0], result_correction(str(y[1][1]))] for y in year]
+    print(gold, silver, bronze)
+    
+elif sport in [14, 15, 16, 17, 18, 20, 21, 22, 36, 37, 38, 39, 40, 42, 43, 44]:
+    for y in year:
+        y[1][0] = float(y[1][0])
+        y[1][1] = float(y[1][1])
+        y[1][2] = float(y[1][2])
+sorted_data = quick_sort(year)
 
-    sorted_data = quick_sort(year)
+years = [data[0] for data in sorted_data]
+gold = []
+silver = []
+bronze = []
 
-    years = [data[0] for data in sorted_data]
-    gold = []
-    silver = []
-    bronze = []
+for n in range(len(sorted_data)):
+    i = n
+    if sorted_data[n][1][0] == 0:
+        if i == 0:
+            while sorted_data[i+1][1][0] == 0:
+                i += 1
+            sorted_data[n][1][0] = sorted_data[i+1][1][0]
+        else:
+            while sorted_data[i-1][1][0] == 0 and i != 0:
+                i -= 1
+            sorted_data[n][1][0] = sorted_data[i-1][1][0]
+    if sorted_data[n][1][1] == 0:
+        if i == 0:
+            while sorted_data[i+1][1][1] == 0:
+                i += 1
+            sorted_data[n][1][1] = sorted_data[i+1][1][1]
+        else:
+            while sorted_data[i-1][1][1] == 0 and i != 0:
+                i -= 1
+            sorted_data[n][1][1] = sorted_data[i-1][1][1]
+    if sorted_data[n][1][2] == 0:
+        if i == 0:
+            while sorted_data[i+1][1][2] == 0:
+                i += 1
+            sorted_data[n][1][2] = sorted_data[i+1][1][2]
+        else:
+            while sorted_data[i-1][1][2] == 0 and i != 0:
+                i -= 1
+            sorted_data[n][1][2] = sorted_data[i-1][1][2]
 
-    for n in range(len(sorted_data)):
-        i = n
-        if sorted_data[n][1][0] == 0:
-            if i == 0:
-                while sorted_data[i+1][1][0] == 0:
-                    i += 1
-                sorted_data[n][1][0] = sorted_data[i+1][1][0]
-            else:
-                while sorted_data[i-1][1][0] == 0 and i != 0:
-                    i -= 1
-                sorted_data[n][1][0] = sorted_data[i-1][1][0]
-        if sorted_data[n][1][1] == 0:
-            if i == 0:
-                while sorted_data[i+1][1][1] == 0:
-                    i += 1
-                sorted_data[n][1][1] = sorted_data[i+1][1][1]
-            else:
-                while sorted_data[i-1][1][1] == 0 and i != 0:
-                    i -= 1
-                sorted_data[n][1][1] = sorted_data[i-1][1][1]
-        if sorted_data[n][1][2] == 0:
-            if i == 0:
-                while sorted_data[i+1][1][2] == 0:
-                    i += 1
-                sorted_data[n][1][2] = sorted_data[i+1][1][2]
-            else:
-                while sorted_data[i-1][1][2] == 0 and i != 0:
-                    i -= 1
-                sorted_data[n][1][2] = sorted_data[i-1][1][2]
+    gold.append(sorted_data[n][1][0])
+    silver.append(sorted_data[n][1][1])
+    bronze.append(sorted_data[n][1][2])
+timetable = pd.DataFrame()
 
+timetable = timetable.assign( #Maakt het tabel met pandas aan
+    year = years,
+    gold = gold,
+    silver = silver,
+    bronze = bronze
+)
 
-        gold.append(sorted_data[n][1][0])
-        silver.append(sorted_data[n][1][1])
-        bronze.append(sorted_data[n][1][2])
+timetable.plot(x="year", y=["gold", "silver", "bronze"], kind="line", marker="o")
+plt.title(data[sport]["name"])
+plt.ylabel("Number of medals")
+plt.show()
 
-
-    timetable = pd.DataFrame()
-    timetable = timetable.assign( #Maakt het tabel met pandas aan
-        year = years,
-        gold = gold,
-        silver = silver,
-        bronze = bronze
-    )
-
-    timetable.plot(x="year", y=["gold", "silver", "bronze"], kind="line", marker="o")
-    plt.title(data[sport]["name"])
-    plt.ylabel("Number of medals")
-    plt.show()
-
-    print(timetable)
+print(timetable)
